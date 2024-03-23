4b0a94f703e66ca1277160be42639d87f35abb20
Luis <150976352+lmcs73@users.noreply.github.com>
2024-03-23 18:03:50 +0000

    csv

--- Ben_t.py
+++ Ben_t.py
@@ -15,19 +15,14 @@ categorical_variables = ['Campaign', 'Batching_1', 'Batching_2', 'Batching_3']
 # Variável dependente
 y = benihana['Profit']
 
-# Garantir que todas as variáveis numéricas são do tipo float
-for col in numeric_variables:
-    benihana[col] = benihana[col].astype(float)
-
 # Pré-processar variáveis categóricas
-# Converter para variáveis dummy
 dummies = pd.get_dummies(benihana[categorical_variables], drop_first=True)
 
 # Juntar as variáveis numéricas e dummies
 X = pd.concat([benihana[numeric_variables], dummies], axis=1)
 
 # Preparar a matriz de design A incluindo um intercepto
-A = np.c_[np.ones(X.shape[0]), X.values]
+A = np.c_[np.ones(X.shape[0]), X]
 
 # Resolver a regressão linear
 C, residuals, rank, s = lstsq(A, y)
